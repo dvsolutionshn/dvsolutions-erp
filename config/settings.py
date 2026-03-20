46afaa75998d366d842db6f0d579ce3019eb4bb5
Daniel Varela <dannyvarela25@gmail.com>
2026-03-19 19:42:38 -0600

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8$@06&%3qnsgdxil4yt9(i6!#z8k+#yoak)36y_jz#320lk9+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
