68ec832d129e9286e18b6d2efc1e18efc8342998
DV Solutions <dv.solutionshn@gmail.com>
2026-06-07 20:19:22 -0600

    Restaurar catalogo y facturacion a estado estable

--- facturacion/models.py
+++ facturacion/models.py
@@ -276,22 +276,6 @@ class Producto(models.Model):
     motivo_eliminacion = models.TextField(blank=True, null=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
-    @property
-    def foto_url(self):
-        if not self.foto:
-            return ""
-        try:
-            return self.foto.url
-        except ValueError:
-            return ""
-
-    @property
-    def perfil_farmaceutico_seguro(self):
-        try:
-            return self.perfil_farmaceutico
-        except PerfilFarmaceuticoProducto.DoesNotExist:
-            return None
-
     def clean(self):
         super().clean()
 
